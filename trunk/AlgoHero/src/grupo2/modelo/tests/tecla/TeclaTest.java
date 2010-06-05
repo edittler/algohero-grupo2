44b0381073cb36c429ab3ab588eb70ee7399c52b
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-05 22:57:25 +0000

    Ya fue solucionado el problema de una de las pruebas de CombinacionDeTeclasTest.

--- trunk/AlgoHero/src/grupo2/modelo/tests/tecla/TeclaTest.java
+++ trunk/AlgoHero/src/grupo2/modelo/tests/tecla/TeclaTest.java
@@ -10,23 +10,15 @@ import grupo2.modelo.tecla.*;
 public class TeclaTest {
 	
 	@Test 
-	  public void testGetTecla(){
+	 public void testGetTecla(){
 		
+		//creo una tecla y le asigno el codigo ASCII correspondiente a 's'
 		Tecla unaTecla = new Tecla(115);
 		
 		//se prueba que  el codigo ASCII ingresado coincida  
 		assertEquals("codigo ASCIII incorrecto", unaTecla.getCodigoASCII(),115);
 		//se prueba que el la tecla devuelta sea la correcta para el codigo ASCII ingresado
 		assertEquals("Tecla incorrecta", unaTecla.getTecla(),'s');
-					
-		
 	}
 	
-	
-	
-	  
-	
-	
-	
-
 }
