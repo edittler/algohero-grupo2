422bef6a11174b9979a7e24ab3b8c317b1b252ff
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-05 01:09:55 +0000

    Implemento un IteradorElementos en la clase Compas que solo contenga los siguientes metodos: * boolean hasNext() * ElementoDeCompas next() Que son los necesarios para el cliente.

--- trunk/AlgoHero/src/grupo2/modelo/Compas.java
+++ trunk/AlgoHero/src/grupo2/modelo/Compas.java
@@ -1,45 +1,38 @@
 package grupo2.modelo;
 
-import grupo2.modelo.excepciones.CompasInvalidoExcepcion;
-import grupo2.modelo.excepciones.CompasSobrepasadoExcepcion;
+import grupo2.modelo.excepciones.*;
 
 import java.util.*;
-
-
-
 
 public class Compas {
 	private int numerador; // representa una cantidad de figuras
 	private int denominador; // representa una figura
-	private Iterator<ElementoDeCompas> itElementos;
-	private ArrayList<ElementoDeCompas> elementos; // almacena notas, silencios
-													// o acordes
+	
+	// almacena notas, silencios o acordes
+	private ArrayList<ElementoDeCompas> elementos;
 
 	// Construcctor por default asigna 4/4
-	public Compas() {
+	public Compas(){
 		this.setNumerador(4);
 		this.setDenominador(4);
 		this.elementos = new ArrayList<ElementoDeCompas>();
-		this.itElementos = this.elementos.iterator();
 	}
-//denominador representa una figura y se escribe como multiplos de Redonda (2 blanca, 4 negra, 8 corchea,etc) 
-	public Compas(int numerador, int denominador) {
+	
+	//denominador representa una figura y se escribe como multiplos de Redonda (2 blanca, 4 negra, 8 corchea,etc) 
+	public Compas(int numerador, int denominador){
 		this.setNumerador(numerador);
 		this.setDenominador(denominador);
 		this.elementos = new ArrayList<ElementoDeCompas>();
-		this.itElementos = this.elementos.iterator();
-				
 	}
 	
-
-	public void setNumerador(int numerador) {
+	public void setNumerador(int numerador){
 		if(numerador<=0){
 			throw new CompasInvalidoExcepcion();
 		}
 		this.numerador = numerador;
 	}
 
-	public int getNumerador() {
+	public int getNumerador(){
 		return numerador;
 	}
 
@@ -60,18 +53,6 @@ public class Compas {
 			throw new CompasSobrepasadoExcepcion();
 		}
 		this.elementos.add(elemento);
-
-	}
-//retorna null si ya se recorrio todo el compas y sino retorna el siguiente elemento
-	public ElementoDeCompas getSiguienteElemento(){
-		ElementoDeCompas elemento=null;
-		if(!this.itElementos.hasNext()){
-			this.itElementos = this.elementos.iterator();
-		}
-		else{
-			elemento=this.itElementos.next();
-		}
-		return elemento;
 	}
 	
 	/*Metodos auxiliares*/
@@ -106,6 +87,34 @@ public class Compas {
 			sumaDuracion += unElemento.getDuracion();
 		}
 		return sumaDuracion;
+	}
+	
+	// CLASE ITERADOR PRIVADA
+	private class IteratorElementos {
+		
+		private Iterator<ElementoDeCompas> itElementos;
+		
+		public IteratorElementos(ArrayList<ElementoDeCompas> unaLista){
+			this.itElementos = unaLista.iterator();
+		}
+		
+		@SuppressWarnings("unused")
+		public boolean hasNext() {
+			
+			return this.itElementos.hasNext();
+		}
+
+		@SuppressWarnings("unused")
+		public ElementoDeCompas next() {
+			
+			return this.itElementos.next();
+		}
+		
+	}
+	
+	public IteratorElementos getIteratorElementos(){
+		IteratorElementos unIterator = new IteratorElementos(this.elementos);
+		return unIterator;
 	}
 
 }
