eef84c36907fd24931b37a931c35f7a0d5830e3c
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-05 22:57:25 +0000

    Ya fue solucionado el problema de una de las pruebas de CombinacionDeTeclasTest.

--- AlgoHero/src/grupo2/modelo/tests/tecla/CombinacionDeTeclasTest.java
+++ AlgoHero/src/grupo2/modelo/tests/tecla/CombinacionDeTeclasTest.java
@@ -1,6 +1,7 @@
 package grupo2.modelo.tests.tecla;
 
 import static org.junit.Assert.assertEquals;
+import java.util.Iterator;
 import org.junit.Test;
 import grupo2.modelo.tecla.*;
 
@@ -11,35 +12,18 @@ public class CombinacionDeTeclasTest {
 	//se crea una combinacion de teclas con el constructor sin parametros
 	CombinacionDeTeclas unaCombinacion = new CombinacionDeTeclas();
 	
-	//creo una tecla para ingresar en la combinacion
+	//creo una tecla 'A' y la agrego en la combinacion
 	Tecla teclaA = new Tecla(65);
-
-	//se agrega tecla en la combinacion 
 	unaCombinacion.agregarTecla(teclaA);
 	
-	//creo otra tecla para ingresar en la combinacion
+	//creo otra tecla 'B' y la agrego en la combinacion
 	Tecla teclaB = new Tecla(66);
-
-	//se agrega tecla en la combinacion 
 	unaCombinacion.agregarTecla(teclaB);
 	
-	assertEquals("Tecla_A invalida", unaCombinacion.getIteradorTeclas().next().getTecla(), 'A');
-	//ver porque falla cuando quiero obtener la segunda tecla ingresada en la combinacion
-	/*assertEquals("Tecla_B invalida", unaCombinacion.getIteradorTeclas().next().getTecla(), 'B');
+	Iterator<Tecla> itTeclas = unaCombinacion.getIteradorTeclas();
 	
-    */
-    
-	
-	
+	assertEquals("Tecla_A invalida", itTeclas.next().getTecla(), 'A');
+	assertEquals("Tecla_B invalida", itTeclas.next().getTecla(), 'B');
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
 }
