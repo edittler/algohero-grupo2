178ee7f0b7412530de94ae8a93ccb147b80dd159
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-08 21:08:31 +0000

    Modificaciones en la clase Cancion: *Se estudiaron los niveles de accesibilidad de los metodos y se establecieron como privados aquellos que se cree que no requieren ser usados por el cliente. *Se agrego un constructor sin parametros para reemplazar el que establece el compilador por default debido a que se requieren que las colecciones esten inicializadas. *Se almacenó el tempo por default como una constante en la clase Constantes. *Se agregaron los test para el constructor por default y se resolvieron los conflictos de los tets que fueron afectados por el cambio de visibilidad de los metodos.

--- AlgoHero/src/grupo2/modelo/Cancion.java
+++ AlgoHero/src/grupo2/modelo/Cancion.java
@@ -2,7 +2,7 @@ package grupo2.modelo;
 
 import java.util.*;
 
-import grupo2.modelo.constantes.Constantes;
+import grupo2.modelo.constantes.*;
 import grupo2.modelo.excepciones.*;
 import grupo2.modelo.nota.Nota;
 import grupo2.modelo.tecla.*;
@@ -10,14 +10,26 @@ import grupo2.modelo.tecla.*;
 public class Cancion {
 
 	private String nombre;
-	private int tempo; // [negras/minuto]
+	private int tempo; //[negras/minuto]
 	private ArrayList<Compas> compaces;
-	private MapaDeTeclas mapeo;// almacanena correspondencias entre un tono (diferenciadopor su frecuencia)
-										// y una combinacion de teclas
+	
+	private MapaDeTeclas mapeo;
+	/* mapeo: almacanena correspondencias entre un tono
+	 * (diferenciado por su frecuencia) y una combinacion de teclas
+	 */
+	
+	// Constructor por defecto sin parametros
+	public Cancion() {
+		this.setNombre("Sin Nombre");
+		this.tempo = Constantes.TEMPO_DEFAULT;
+		this.compaces = new ArrayList<Compas>();
+		this.mapeo = new MapaDeTeclas();
+	}
+	
 	// Constructor que asigna nombre a la cancion e inicializa colecciones
 	public Cancion(String Nombre) {
 		this.setNombre(Nombre);
-		this.tempo = 90;
+		this.tempo = Constantes.TEMPO_DEFAULT;
 		this.compaces = new ArrayList<Compas>();
 		this.mapeo = new MapaDeTeclas();
 	}
@@ -29,7 +41,10 @@ public class Cancion {
 	public String getNombre() {
 		return nombre;
 	}
-// 30<= tempo <= 150
+	
+	/* Pre-condicion: El tempo debe estar en un valor entre 30 y 150
+	 * Post-concicion: Si no se cumple la pre-condicion, se lanza una excepcion
+	 */
 	public void setTempo(int tempo)
 	{
 		if ((tempo<30)||(tempo>150)){
@@ -42,10 +57,9 @@ public class Cancion {
 		return tempo;
 	}
 	
-	public MapaDeTeclas getMapeo(){
-		return mapeo;
-	}
-	
+	/* Pre-condicion: El compas a agregar debe estar completo (en terminos del tiempo)
+	 * Post-concicion: Si no se cumple la pre-condicion, se lanza una excepcion
+	 */
 	public void agregarCompas(Compas unCompas) {
 		if (unCompas.estaInconcluso()){
 			throw new CompasInvalidoExcepcion();
@@ -57,50 +71,63 @@ public class Cancion {
 		this.mapeo.agregarMapeo(nota, combinacion);
 	}
 
-	public Compas getCompas(int index){
-		return this.compaces.get(index);
-	}
 	
-	//devuelve el elemento de la cancion correspondiente al instante ingresado
+	/* Post-condicion: devuelve el elemento de la cancion correspondiente
+	 * al instante ingresado
+	 */
 	public ElementoDeCompas getElemento(double instante){
 		Iterator<Compas> itCompaces=this.getIteratorCompaces();
 		double LineaDeTiempo = 0.00; //Cuenta el tiempo transcurrido total a medida q se recorre la cancion
 		boolean entro= false; //marca cuando entra en el rango de presicion entorno al elemento buscado 
 		int compas=0; //cuenta los compaces recorridos
 		int elemento=0; //cuenta los elementos recorridos (correspondiente a un compas)
+		
 		//Recorro los compaces hasta llegar al instante ingresado
-		while(!entro&&itCompaces.hasNext()){
+		while(!entro && itCompaces.hasNext()){
 			Compas unCompas = itCompaces.next();
 			Iterator<ElementoDeCompas> itElementos = unCompas.getIteratorElementos();
 			elemento=0;
-			while(itElementos.hasNext()&&!entro){
-				ElementoDeCompas unElemento=itElementos.next();
-				entro=this.entraEnElRangoDePresicion(LineaDeTiempo, instante); //"entra dentro del rango?"
+			
+			while(itElementos.hasNext() && !entro){
+				ElementoDeCompas unElemento = itElementos.next();
+				entro = this.entraEnElRangoDePresicion(LineaDeTiempo, instante); //"entra dentro del rango?"
 				elemento++; 
 				LineaDeTiempo += (double)(unElemento.getDuracion()/((this.getTempo()/60.00))); 
 			}
 			compas++;
-			elemento--;
+			elemento--; // para que se realiza esta operacion si cuando
+						// se reinicia el ciclo el valor vuelve a 0?
 		}
-		compas--;
+		
+		compas--; // idem con la pregunta de elemento--
 		return this.getCompas(compas).getElemento(elemento);
 	}
 	
-	
-	
-	public Iterator<Compas> getIteratorCompaces(){
-		IteradorCliente<Compas> unIterator = new IteradorCliente<Compas>(this.compaces);
-		return unIterator;
-	}
-
-	//Recive una combinacion de teclas y un instante en que fueron presionadas y devuelve true si 
-	//coincide con el mapeo de la cancion en el instante indicado.
-	
+	/* Pre-condicion: Recibe una combinacion de teclas y un instante en que fueron presionadas
+	 * Post-condicion: devuelve true si coincide con el mapeo de la cancion en el instante indicado.
+	 */
 	public boolean chequear(CombinacionDeTeclas teclasPresionadas, double instante) {
 		ElementoDeCompas unElemento=this.getElemento(instante); 
 		return unElemento.chequear(this.getMapeo(), teclasPresionadas);
 	}
-/*** método auxiliar***/
+	
+	
+	/***
+	 *  METODOS AUXILIARES INTERNOS
+	 ***/
+	
+	private MapaDeTeclas getMapeo(){
+		return mapeo;
+	}
+	
+	private Iterator<Compas> getIteratorCompaces(){
+		IteradorCliente<Compas> unIterator = new IteradorCliente<Compas>(this.compaces);
+		return unIterator;
+	}
+	
+	private Compas getCompas(int index){
+		return this.compaces.get(index);
+	}
 	
 	//devuelve true si el valor ingresado se encuentra dentro del rango de presicion entorno a entorno segun la presicion del juego 
 	private boolean entraEnElRangoDePresicion(double valor,double entorno){ 
