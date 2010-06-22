f4ad1dd625368a28f1078c983ad955df7b01921f
em2anuel371 <em2anuel371@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-22 03:52:48 +0000

    Agregue un par de clases en vista, cuando se pone nuevo juego entonces aparece el panel de juego. Tienen que subir el titiritero no se porque no pude en este proyecto.

---
+++ AlgoHero/src/algo3c1g2/controlador/InicioJuego.java
@@ -0,0 +1,44 @@
+package algo3c1g2.controlador;
+
+import algo3c1g2.vista.*;
+import ar.uba.fi.algo3.titiritero.ControladorJuego;
+import ar.uba.fi.algo3.titiritero.ObjetoVivo;
+import ar.uba.fi.algo3.titiritero.SuperficieDeDibujo;
+
+
+public class InicioJuego implements ObjetoVivo{
+	
+	private ControladorJuego controlador = null;
+	
+	
+	
+	public InicioJuego(SuperficieDeDibujo superficieDeDibujo){
+		Pentagrama pentagrama = new Pentagrama(600,600);
+		
+		controlador = new ControladorJuego();
+		controlador.setSuperficieDeDibujo(superficieDeDibujo);
+		
+		VistaPentagrama vistaPentagrama = new VistaPentagrama();
+		vistaPentagrama.setPosicionable(pentagrama);
+		controlador.agregarDibujable(vistaPentagrama);
+		controlador.setIntervaloSimulacion(20);
+			
+	}
+	
+	
+	public void comenzar(){
+		controlador.comenzarJuegoAsyn();	
+	}
+
+	public void detener() {
+		controlador.detenerJuego();
+	}
+
+
+	@Override
+	public void vivir() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
