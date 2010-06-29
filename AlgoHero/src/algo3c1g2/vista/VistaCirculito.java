254c434fe5a388e79132725fc3b86ab45ce0b204
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-06-29 22:28:32 +0000

    Elimino los warnings en las siguientes clases para que no nos asustemos.

--- AlgoHero/src/algo3c1g2/vista/VistaCirculito.java
+++ AlgoHero/src/algo3c1g2/vista/VistaCirculito.java
@@ -3,7 +3,6 @@ package algo3c1g2.vista;
 import java.awt.Color;
 
 import ar.uba.fi.algo3.titiritero.vista.Circulo;
-import ar.uba.fi.algo3.titiritero.vista.Imagen;
 
 public class VistaCirculito extends Circulo {
 
