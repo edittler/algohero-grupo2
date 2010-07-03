1217eb421264a2ddcef9c1d8270163163c323157
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-07-03 23:29:24 +0000

    Cambio la clase Puntaje para que no dependa de Cancion y seguimos en la busqueda del error

--- AlgoHero/src/algo3c1g2/controlador/Escuchador.java
+++ AlgoHero/src/algo3c1g2/controlador/Escuchador.java
@@ -3,15 +3,12 @@ package algo3c1g2.controlador;
 import java.awt.event.KeyEvent;
 
 import algo3c1g2.modelo.Guitarra;
-import algo3c1g2.modelo.Puntaje;
 import ar.uba.fi.algo3.titiritero.KeyPressedObservador;
 
 public class Escuchador implements KeyPressedObservador {
 
 	private Guitarra guitarra;
-	private Puntaje puntaje;
 	private boolean estaPresionado;
-	
 
 	public Escuchador(Guitarra guitarra) {
 		this.guitarra = guitarra;
@@ -21,13 +18,12 @@ public class Escuchador implements KeyPressedObservador {
 	public void keyPressed(KeyEvent event) {
 		if (!this.estaPresionado) {
 			this.estaPresionado = true;
-			boolean acerto=this.puntaje.contarPuntos(this.guitarra.getInstanteDeCancion(),((char) event.getKeyCode()));
-			if(acerto){
-			this.guitarra.reproducir(this.guitarra.getInstanteDeCancion());
+			boolean acerto = this.guitarra.contarPuntos(event.getKeyCode());
+			if (acerto) {
+				this.guitarra.reproducir(this.guitarra.getInstanteDeCancion());
 			}
-			
+
 		} else {
-			  
 			this.estaPresionado = false;
 		}
 	}
--- AlgoHero/src/algo3c1g2/modelo/Puntaje.java
+++ AlgoHero/src/algo3c1g2/modelo/Puntaje.java
@@ -1,19 +1,15 @@
 package algo3c1g2.modelo;
 
-import algo3c1g2.modelo.tecla.CombinacionDeTeclas;
-import algo3c1g2.modelo.tecla.Tecla;
 import ar.uba.fi.algo3.titiritero.Posicionable;
 
 public class Puntaje implements Posicionable {
 
 	private int puntos;
-	private Cancion cancion;
-	private static final double PRESICION_DEL_JUEGO=0.5; //segundos
 	
-	public Puntaje(){
-		this.puntos= 0;
+	public Puntaje() {
+		this.puntos = 0;
 	}
-	
+
 	@Override
 	public int getX() {
 		return 0;
@@ -23,31 +19,13 @@ public class Puntaje implements Posicionable {
 	public int getY() {
 		return 0;
 	}
-	
-	public int getPuntos(){
+
+	public int getPuntos() {
 		return this.puntos;
 	}
 
-	public boolean contarPuntos(double instante, char tecla){
-		CombinacionDeTeclas TeclasPresionadas=new CombinacionDeTeclas();
-		Tecla unaTecla=new Tecla(tecla);
-		boolean acerto=false;
-	
-		TeclasPresionadas.agregarTecla(unaTecla);
-		acerto= this.cancion.verificarTeclas(TeclasPresionadas, instante, PRESICION_DEL_JUEGO);
-			this.puntos=(acerto)?
-					this.puntos+=1000
-					:this.puntos;
-			return true;
-		}
-		
+	public void sumarPuntos() {
+		this.puntos = this.puntos + 1000;
+	}
 
-		
-
-	
-	
-	
-					
-					
-	
 }
