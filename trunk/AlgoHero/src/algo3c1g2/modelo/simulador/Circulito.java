e86b018797fd20acffae82320863e75291445d93
jibadano <jibadano@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-07-04 20:50:23 +0000

    separe la vista del modelo en Circulito

--- trunk/AlgoHero/src/algo3c1g2/modelo/simulador/Circulito.java
+++ trunk/AlgoHero/src/algo3c1g2/modelo/simulador/Circulito.java
@@ -1,26 +1,23 @@
 package algo3c1g2.modelo.simulador;
 
 
-import algo3c1g2.vista.ventanas.VentanaPrincipal;
 import ar.uba.fi.algo3.titiritero.ObjetoVivo;
 import ar.uba.fi.algo3.titiritero.Posicionable;
 
 public class Circulito implements ObjetoVivo, Posicionable {
 
-	private static final int POSICION_INICIAL_Y = -85;
 
 	private int x;
 	private int y;
 	private boolean habilitado;
 	private boolean fueReproducido;
 	private double instanteASerReproducido;
-	public static int pixelesPorCiclo;
 	private int numeroDeCuerda;
 	private String teclas;
 
 	public Circulito(int numeroDeCuerda) {
 		this.setNumeroDeCuerda(numeroDeCuerda);
-		this.y = POSICION_INICIAL_Y;
+		this.y = 0;
 		this.habilitado = false;
 		this.fueReproducido = false;
 		this.teclas="";
@@ -37,6 +34,29 @@ public class Circulito implements ObjetoVivo, Posicionable {
 	public void habilitar() {
 		this.habilitado = true;
 	}
+
+	public void reiniciar() {
+		this.y = 0;
+		this.habilitado = false;
+		this.fueReproducido = false;
+		this.teclas="";
+	}
+
+	@Override
+	public void vivir() {
+		this.y+=(this.habilitado)? 1 : 0;
+	}
+
+	@Override
+	public int getX() {
+		return this.x;
+	}
+
+	@Override
+	public int getY() {
+		return this.y;
+	}
+
 	
 	public void establecerQueFueReproducido() {
 		this.fueReproducido = true;
@@ -86,35 +106,6 @@ public class Circulito implements ObjetoVivo, Posicionable {
 		&&(((this.getInstanteASerReproducido()-presicion)<=instante)));
 	}
 
-	public void reiniciar() {
-		this.y = POSICION_INICIAL_Y;
-		this.habilitado = false;
-		this.fueReproducido = false;
-		this.teclas="";
-	}
 
-	@Override
-	public void vivir() {
-		if (habilitado) {
-			this.y = this.y + 2;
-		}
-		if (this.y > VentanaPrincipal.TAMAÑO_VERTICAL) {
-			this.reiniciar();
-		}
-	}
-	
-	/*********************************************
-	 * ATRIBUTOS CORRESPONDIENTES A POSICIONABLE *
-	 *********************************************/
-
-	@Override
-	public int getX() {
-		return this.x;
-	}
-
-	@Override
-	public int getY() {
-		return this.y;
-	}
 
 }
