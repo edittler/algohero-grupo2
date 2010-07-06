af9a07f7a068fc3bc07d089720638abc69652227
ezeperez26 <ezeperez26@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-07-06 06:45:35 +0000

    Comente con JavaDoc completamente las clases del persistidor.

--- AlgoHero/src/algo3c1g2/persistencia/PersistidorCancion.java
+++ AlgoHero/src/algo3c1g2/persistencia/PersistidorCancion.java
@@ -19,9 +19,20 @@ import algo3c1g2.modelo.Cancion;
 import algo3c1g2.modelo.Compas;
 import algo3c1g2.modelo.excepciones.CompasInvalidoExcepcion;
 
+/**
+ * Clase de persistencia externa de la {@code Cancion}.
+ * Utiliza las librerias JDOM.
+ * 
+ * @author Ezequiel Pérez Dittler
+ */
 public class PersistidorCancion {
-
 	
+	/**
+	 * Persiste la cancion, sus compaces, notas y el mapeo asociado.
+	 * 
+	 * @param unaCancion {@code Cancion} a persistir.
+	 * @return Retorna el {@code Element} correspondiente a un nodo XML.
+	 */
 	@SuppressWarnings({ "unchecked" })
 	public Element persistir(Cancion unaCancion){
 		//Nombra al elemento raiz con el nombre de la clase
@@ -52,8 +63,10 @@ public class PersistidorCancion {
 	}
 	
 	/**
+	 * Persiste la cancion, sus compaces, notas, el mapeo asociado
+	 * y la almacena en la ruta especificada.
 	 * 
-	 * @param unaCancion Cancion a persistir
+	 * @param unaCancion Cancion a persistir.
 	 * @param rutaArchivo Ruta del archivo que se debe ingresar con el siguiente formato: "archivo.xml"
 	 */
 	public void persistirYGuardar(Cancion unaCancion, String rutaArchivo){
@@ -69,8 +82,17 @@ public class PersistidorCancion {
 		}
 	}
 	
+	/**
+	 * Carga {@code Cancion} a partir de un archivo XML.
+	 * 
+	 * @param rutaArchivo Ruta del archivo XML donde se encuentra almacenada
+	 * 					  la {@code Cancion} almacenada.
+	 * 					  El formato debe ser: "archivo.xml"
+	 * @return Si no hubo errores, devuelve la {@code Cancion}, sino retorna un elemento nulo.
+	 * @throws FileNotFoundException En caso que no encuentre el archivo especificado.
+	 */
 	@SuppressWarnings("unchecked")
-	public Cancion cargarCancion(String rutaArchivo){
+	public Cancion cargarCancion(String rutaArchivo) throws FileNotFoundException{
 		try {
 			// Obtengo el Documento del Archivo y lo proceso
 			SAXBuilder builder = new SAXBuilder();
@@ -106,23 +128,23 @@ public class PersistidorCancion {
 			 */
 			return unaCancion;
 		
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		} catch (DataConversionException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		} catch (CompasInvalidoExcepcion e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		} catch (JDOMException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			return null;
 		}
-		return null;
 	}
 	
 	
--- AlgoHero/src/algo3c1g2/persistencia/PersistidorElemento.java
+++ AlgoHero/src/algo3c1g2/persistencia/PersistidorElemento.java
@@ -1,15 +1,27 @@
 package algo3c1g2.persistencia;
 
+import org.jdom.DataConversionException;
 import org.jdom.Element;
 
 import algo3c1g2.modelo.ElementoDeCompas;
 import algo3c1g2.modelo.nota.*;
 import algo3c1g2.modelo.figura.*;
 
-
+/**
+ * Clase de persistencia externa del {@code ElementoDeCompas}.
+ * Utiliza las librerias JDOM.
+ * 
+ * @author Ezequiel Pérez Dittler
+ */
 @SuppressWarnings("unused")
 public class PersistidorElemento {
 
+	/**
+	 * Persiste el {@code ElementoDeCompas} y sus atributos.
+	 * 
+	 * @param unElemento {@code ElementoDeCompas} a persistir.
+	 * @return Retorna el {@code Element} correspondiente a un nodo XML.
+	 */
 	public Element persistir(ElementoDeCompas unElemento) {
 		Element root = new Element("ElementoDeCompas");
 		root.setText(unElemento.getClass().getName());
@@ -24,10 +36,17 @@ public class PersistidorElemento {
 		return root;
 	}
 
+	/**
+	 * Obtiene el {@code ElementoDeCompas} a traves de un {@code Element}.
+	 * 
+	 * @param root Nodo raiz que deberia contener el {@code ElementoDeCompas}.
+	 * @return Devuelve el {@code ElementoDeCompas} obtenido.
+	 * @throws DataConversionException Si el elemento no tiene {@code ElementoDeCompas}.
+	 */
 	@SuppressWarnings("unchecked")
-	public ElementoDeCompas obtenerElemento(Element unElemento) {
+	public ElementoDeCompas obtenerElemento(Element root) {
 		try {
-			String nombreClase = unElemento.getTextNormalize();
+			String nombreClase = root.getTextNormalize();
 			
 			/*Creo una Clase cuyo nombre esta almacenado en el Elemento
 			 * e instancio el objeto.
@@ -37,7 +56,7 @@ public class PersistidorElemento {
 			
 			/* Ahora obtengo la figura y la agrego al elemento de Compas
 			 */
-			Element elementoFigura = (Element) unElemento.getChild("Figura");
+			Element elementoFigura = (Element) root.getChild("Figura");
 			String nombreFigura = elementoFigura.getTextNormalize();
 			Class unaClaseFigura = Class.forName(nombreFigura);
 			Figura unaFigura = (Figura) unaClaseFigura.newInstance();
