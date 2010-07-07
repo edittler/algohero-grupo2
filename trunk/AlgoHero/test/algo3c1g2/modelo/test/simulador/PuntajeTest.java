fbcde16153b8d3d9eb2fe0b0da1758a9841b5cb9
jibadano <jibadano@bcf34c1f-ea69-ff3a-1735-bc66beaa8d1d>
2010-07-07 18:42:22 +0000

    arregle un par de cositas y subo los diagramas por favor imprimanlos!

--- trunk/AlgoHero/test/algo3c1g2/modelo/test/simulador/PuntajeTest.java
+++ trunk/AlgoHero/test/algo3c1g2/modelo/test/simulador/PuntajeTest.java
@@ -5,7 +5,6 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 import algo3c1g2.modelo.Cancion;
-import algo3c1g2.modelo.nota.Nota;
 import algo3c1g2.modelo.simulador.Puntaje;
 
 public class PuntajeTest {
@@ -21,26 +20,6 @@ public class PuntajeTest {
 		assertEquals(unPuntaje.getPuntos(),0);
 	}
 	
-
-	@Test
-	public void testGetElementoAReproducirConCancionNula() {
-		Cancion unaCancion = null;
-		Puntaje unPuntaje = new Puntaje(unaCancion);
-		
-		Nota unaNota = unPuntaje.getElementoAReproducir(40);
-		
-		assertTrue(unaNota==null);
-	}
-	
-	@Test
-	public void testGetElementoAReproducirConCancionSinNotas() {
-		Cancion unaCancion = new Cancion("MiCancion");
-		Puntaje unPuntaje = new Puntaje(unaCancion);
-		
-		Nota unaNota = unPuntaje.getElementoAReproducir(40);
-		
-		assertTrue(unaNota==null);
-	}
 
 	@Test
 	public void testContarPuntosConCancionNula() {
